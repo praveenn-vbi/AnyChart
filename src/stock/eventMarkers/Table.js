d5c4edd95a4efab1304bae9407b0ad9fe469b72b
Ilya Kurnoy <ilyakurnnoj@gmail.com>
2018-08-03 16:46:45 +0800

    fixed sudden in scope variable override

--- src/stock/eventMarkers/Table.js
+++ src/stock/eventMarkers/Table.js
@@ -215,19 +215,19 @@ anychart.stockModule.eventMarkers.Table.prototype.getIterator = function(coItera
       var prevKey = NaN;
       var prevIndex = NaN;
       var lookup = 0;
-      var firstIndex = NaN;
+      var firstIndex2 = NaN;
       from = isNaN(fromOrNaNForFull) ? -Infinity : fromOrNaNForFull;
       to = isNaN(toOrNaNForFull) ? +Infinity : toOrNaNForFull;
       while (coIterator.advance()) {
         var currentKey = coIterator.currentKey();
         var currentIndex = coIterator.currentIndex();
-        if (isNaN(firstIndex))
-          firstIndex = currentIndex;
+        if (isNaN(firstIndex2))
+          firstIndex2 = currentIndex;
         var from, to;
         var diff = (currentKey - prevKey) / 2;
         for (var i = 0; i < this.data_.length; i++) {
           var keyInsideBounds = this.data_[i].key < to && this.data_[i].key > from;
-          var keyInsideFirstVisible = (prevIndex == firstIndex) && this.data_[i].key >= from && this.data_[i].key < (prevKey + diff);
+          var keyInsideFirstVisible = (prevIndex == firstIndex2) && this.data_[i].key >= from && this.data_[i].key < (prevKey + diff);
           var keyInsidePreviousVisible = false;//this.data_[i].key <= (currentKey - diff) && this.data_[i].key >= (prevKey - diff) && !isNaN(prevKey);
           var keyInsideCurrent = this.data_[i].key <= (currentKey + diff) && this.data_[i].key >= (currentKey - diff) && !isNaN(prevKey);
           var keyInsideLastVisible = false;//currentIndex == coIterator.getRowsCount() && this.data_[i].key <= to && this.data_[i].key > (currentKey - diff);
