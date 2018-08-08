4504252097d71cc8035d34031782a5a8c578eb6d
Ilya Kurnoy <ilyakurnnoj@gmail.com>
2018-08-08 15:45:54 +0800

    apply pixelShift to pixel pairs

--- src/radarPolarBase/Series.js
+++ src/radarPolarBase/Series.js
@@ -221,9 +221,18 @@ anychart.radarPolarBaseModule.Series.prototype.ratiosToPixelPairs = function(x, 
   for (var i = 0; i < ys.length; i++) {
     var y = ys[i];
     var angle = anychart.math.round(goog.math.toRadians(goog.math.modulo(startAngle - 90 + 360 * x, 360)), 4);
+    var yPixelShift = 0;
+    var xPixelShift = 0;
+    var angleDegrees = goog.math.modulo(startAngle - 90 + 360 * x, 360);
+    if (angleDegrees == 90) {
+      xPixelShift = -.5;
+    } else if (angleDegrees == 270) {
+      xPixelShift = .5;
+    }
+
     var radius = this.innerRadius + (this.radius - this.innerRadius) * y;
     result.push(
-        this.cx + radius * Math.cos(angle),
+        this.cx + radius * Math.cos(angle) + xPixelShift,
         this.cy + radius * Math.sin(angle)
     );
   }
